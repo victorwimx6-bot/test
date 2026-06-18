795f887bc41161a8aee1edc58770084a2ebd5dec
victor <victorwimx6@gmail.com>
2026-06-18 17:00:54 -0600

    fix(carousel, home): cambiando de lugar la data de anuncios

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,13 +1,8 @@
 // src/app/page.tsx
 import Carousel from "../components/Carousel";
 import PhoneShowcase from "../components/PhoneShowcase";
+import { images } from "../data/anuncios";
 
-// Tipado explícito
-const images: string[] = [
-  "https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&auto=format&fit=crop&w=2762&q=80",
-  "https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&auto=format&fit=crop&w=2762&q=80",
-  "https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&auto=format&fit=crop&w=2762&q=80",
-];
 
 export default function Home() {
   return (
